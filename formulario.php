747e4f66a4dc6c92eb67a5343198ffea6c472b37
Pedro Henrique <amarcilene207@gmail.com>
2024-10-22 21:21:54 -0300

    Prática

--- formulario.php
+++ formulario.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
+<head> 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulário</title>
@@ -18,9 +18,16 @@
     <?php
         $nome = '';
 
-      if(isset($_POST['nome'])) {
-       echo '<p>Olá ' . $nome.! Sejam bem-vindo(a)'</p>'
-      $nome = $POST{'nome'};
+      if(isset($_POST['nome'])) { //isset é uma função do PHP que verifica se há valor numa variável
+
+      //Estamos atribuindo o valor do campo "nome" digitado no formulário
+      //A nossa variávela chamada $nome
+     $nome = $POST{'nome'};
+
+     //Estamoos imprimindo á mensagem de boas vindas
+     //Concatenamos uma string constante com a variável "$nome"
+      echo '<p>Olá,' , $nome .'! Seja bem-vindo(a)<p/>';
+
       }
     <?>
 </body>
